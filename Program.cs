c949428ced96113f838d38229114f4a8a6c29f79
unknown <legiathinh0508@gmail.com>
2024-08-16 21:02:29 +0700

    100SNT

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int num = 2; num < 100; num++)
+        {
+            bool isPrime = true;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
